d7e24f7978531bf2fdd563f192673d0f2341c744
Jobove <OYCuBa@gmail.com>
2022-08-02 21:30:37 +0800

    First test.

--- backend/src/main/java/com/jobove/learningspringboot/controller/pk/BotInfo.java
+++ backend/src/main/java/com/jobove/learningspringboot/controller/pk/BotInfo.java
@@ -3,12 +3,20 @@ package com.jobove.learningspringboot.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfo {
 
     @RequestMapping("getbotinfo/")
-    public String getBotInfo() {
-        return "hhhh";
+    public Map<String, String> getBotInfo() {
+        Map<String, String> res = new HashMap<>();
+
+        res.put("name", "Bot1");
+        res.put("rating", "1000");
+
+        return res;
     }
 }
