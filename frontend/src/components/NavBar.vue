23826c062c47f25dbcfba5bfee863eb3c7bcb926
Jobove <OYCuBa@gmail.com>
2022-08-06 23:23:54 +0800

    Updated navigation bar and card view.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,34 +1,39 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
       <router-link class="navbar-brand" :to="{ name: 'home' }">King of Bots</router-link>
 
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'pk_index' }">对战</router-link>
+            <router-link :class="route_name == 'pk_index' ? 'nav-link active' : 'nav-link'" :to="{ name: 'pk_index' }">对战
+            </router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'ranklist_index' }">天梯榜</router-link>
+            <router-link :class="route_name == 'ranklist_index' ? 'nav-link active' : 'nav-link'"
+              :to="{ name: 'ranklist_index' }">天梯榜</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'record_index' }">对战记录</router-link>
+            <router-link :class="route_name == 'record_index' ? 'nav-link active' : 'nav-link'"
+              :to="{ name: 'record_index' }">对战记录</router-link>
           </li>
         </ul>
 
-        <span class="navbar-text dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Kasugano_Sora
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <router-link class="dropdown-item" :to="{ name: 'character_index' }">我的Bot</router-link>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">退出</a></li>
-          </ul>
+        <span class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Kasugano_Sora
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <router-link class="dropdown-item" :to="{ name: 'character_index' }">我的Bot</router-link>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">退出</a></li>
+            </ul>
+          </li>
         </span>
       </div>
     </div>
@@ -36,7 +41,18 @@
 </template>
 
 <script>
+import { useRoute } from "vue-router";
+import { computed } from "vue";
 
+export default {
+  setup() {
+    const route = useRoute();
+    let route_name = computed(() => route.name);
+    return {
+      route_name
+    }
+  }
+}
 </script>
 
 <style scoped>
--- frontend/src/views/ranklist/RanklistIndexView.vue
+++ frontend/src/views/ranklist/RanklistIndexView.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>Ranklist</div>
+    <ContentField>
+        Ranklist
+    </ContentField>
 </template>
 
 <script>
+import ContentField from "@/components/ContentField.vue"
 
+export default {
+    components: {
+        ContentField
+    }
+}
 </script>
 
 <style scoped>
